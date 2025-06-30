1596b3093189931946db8cc3d523d25dd1e9969d
hwjoo <jhw664089@gmail.com>
2025-06-30 19:11:47 +0900

    feat: :rocket: initialize sauron backend

---
+++ src/test/java/com/sauron/sauron_backend/SauronBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sauron.sauron_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauronBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
